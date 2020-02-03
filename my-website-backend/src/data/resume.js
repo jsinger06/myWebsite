6a7d344489cd8cb33a398ce433da563406834190
jsinger06 <joshua.j.singer79@gmail.com>
2020-02-03 20:42:55 +0000

    initial

---
+++ my-website-backend/src/data/resume.js
@@ -0,0 +1,6 @@
+const resumeData = {
+    "test": "This is the resume data",
+    "certificationsList": ["Pega PRPC CSSA (2016)", "Pega PRPC CSA (2011)"]
+}
+
+export default resumeData;
---
+++ my-website-backend/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import resumeData from './data/resume';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('received hello');
+    res.status(200).send("hello World!");
+});
+
+app.get('/test', (req, res) => {
+    console.log('received test');
+    res.status(200).send("test");
+});
+
+app.get('/api/resume', (req, res) => {
+    console.log('received resume');
+    console.log(resumeData);
+    res.status(200).json(resumeData);
+});
+
+// AWS Cloud 9 can only preview ports 8080, 8081, and 8082
+// Example Preview: https://2ac254789e6d4dcfb9c2be8a67df610d.vfs.cloud9.us-east-2.amazonaws.com:8081/
+app.listen(8081, () => console.log('listening on port 8081'));
