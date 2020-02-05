60e994a783362e37ee50b8030e772b05b76ed241
jsinger06 <48264958+jsinger06@users.noreply.github.com>
2020-02-05 10:19:20 -0500

    React wip (#2)
    * wip
    
    * created AccomplishmentsSection, StrengthSection, and WorkExperienceSection modules. WorkExperienceSection module is still WIP
    
    * Finished WorkExperienceSection and performed some clean up in App.js and StrengthSection.js
    
    * Clean up '&' that was coded
    
    * clean up
    
    * Initial changes for adding Technical Expertise section
    
    * Initial changes for adding Technical Expertise section
    
    * Removed tech expertise section since it is WIP
    
    * cleanup from create-react-app
    
    * Finished styling changes for Technical Expertise section
    
    * Updated software list
    
    * Modularized Education and Certifications sections
    
    * Fixed Tech Expertise sections responsiveness
    
    * fix duplicate border in tech expertise section
    
    * update react-scripts version for running in AWS cloud 9
    
    * import fetch
    
    * update tech list
    
    * Connected to backend and sourced certification list from api
    
    * initial
    
    * clean up test endpoints
    
    * moved data for remaining sections to node server
    
    * prep for build
    
    * build react app and include in node

--- my-website-backend/src/server.js
+++ my-website-backend/src/server.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import path from 'path';
 import resumeData from './data/resume';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('/api/resume', (req, res) => {
     console.log('received resume');
     res.status(200).json(resumeData);
 });
+
+app.get('/resume', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'))
+})
+
+app.get('*', (req, res) => {
+    console.log('redirect')
+    // res.redirect(301, '/resume')
+    res.redirect(301, '/resume')
+})
 
 // AWS Cloud 9 can only preview ports 8080, 8081, and 8082
 // Example Preview: https://2ac254789e6d4dcfb9c2be8a67df610d.vfs.cloud9.us-east-2.amazonaws.com:8081/
