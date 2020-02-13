24917e0fe47e5a5d18abb259052877fe606a0aa6
jsinger06 <48264958+jsinger06@users.noreply.github.com>
2020-02-13 07:22:23 -0500

    New build file for front end

---
+++ my-website-backend/build/precache-manifest.aa282b20d4dbf7a1dabba47a780ec2b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f164f8de4f00efbdc7963391b3295059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf0967bcd6909381c10",
+    "url": "/static/css/main.46e1c0ed.chunk.css"
+  },
+  {
+    "revision": "5d85360ffac12c518f73",
+    "url": "/static/js/2.a7fcc100.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a7fcc100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf0967bcd6909381c10",
+    "url": "/static/js/main.b1274f39.chunk.js"
+  },
+  {
+    "revision": "931686523e4ddd616378",
+    "url": "/static/js/runtime-main.92e1f69d.js"
+  },
+  {
+    "revision": "0d499d50f8765674b779288d7b84699b",
+    "url": "/static/media/joanna-kosinska-unsplash.0d499d50.jpg"
+  }
+]);
