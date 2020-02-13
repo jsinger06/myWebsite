1ca4340e143376b0fd0ef37f914fac43a1ff9278
jsinger06 <joshua.j.singer79@gmail.com>
2020-02-13 20:12:04 +0000

    add try/catch

--- my-website-backend/src/data/resume.js
+++ my-website-backend/src/data/resume.js
@@ -13,76 +13,81 @@ const workExperienceFactory = (company) => ({
 });
 
 const resumeData = async() => {
-    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
-    const db = client.db('myWebsite');
-    const data = await db.collection(collection).find().toArray();
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db('myWebsite');
+        const data = await db.collection(collection).find().toArray();
 
-    let companies = [];
-    let workExperience = {};
+        let companies = [];
+        let workExperience = {};
 
-    // Build Work experience section
-    data.forEach(item => {
-        // Create object containing all companies
-        let obj = {};
-        if (item.company != null && typeof item.company != 'undefined' && !companies.includes(item.company)) {
-            companies.push(item.company);
-            obj[item.company] = workExperienceFactory(item.company);
-        }
-        else {
-            obj[item.company] = workExperience[item.company]
-        }
+        // Build Work experience section
+        data.forEach(item => {
+            // Create object containing all companies
+            let obj = {};
+            if (item.company != null && typeof item.company != 'undefined' && !companies.includes(item.company)) {
+                companies.push(item.company);
+                obj[item.company] = workExperienceFactory(item.company);
+            }
+            else {
+                obj[item.company] = workExperience[item.company]
+            }
 
-        // Build out work experience object
-        if (item.category === 'tenure') {
-            obj[item.company].roles.push({ title: item.jobRole, dates: item.value });
-        }
-        else if (item.category === 'jobDescription') {
-            obj[item.company].jobDescription.description = item.value;
-        }
-        else if (item.category === 'accomplishment') {
-            obj[item.company].jobDescription.accomplishments.push(item.value)
-        }
+            // Build out work experience object
+            if (item.category === 'tenure') {
+                obj[item.company].roles.push({ title: item.jobRole, dates: item.value });
+            }
+            else if (item.category === 'jobDescription') {
+                obj[item.company].jobDescription.description = item.value;
+            }
+            else if (item.category === 'accomplishment') {
+                obj[item.company].jobDescription.accomplishments.push(item.value)
+            }
 
-        if (Object.keys(obj) != 'undefined') {
-            workExperience[item.company] = obj[item.company]
-        }
+            if (Object.keys(obj) != 'undefined') {
+                workExperience[item.company] = obj[item.company]
+            }
 
-    });
+        });
 
-    const resultObj = {
-        accomplishmentsList: [],
-        strengthList: [],
-        certificationsList: [],
-        techExpertiseList: { languages: [], frameworks: [], software: [], servers: [], databases: [] },
-        workExperienceList: Object.values(workExperience)
-    };
+        const resultObj = {
+            accomplishmentsList: [],
+            strengthList: [],
+            certificationsList: [],
+            techExpertiseList: { languages: [], frameworks: [], software: [], servers: [], databases: [] },
+            workExperienceList: Object.values(workExperience)
+        };
 
-    // Build remaining sections
-    data.reduce((acc, item) => {
+        // Build remaining sections
+        data.reduce((acc, item) => {
 
-        let section = null;
+            let section = null;
 
-        switch (item.category) {
-            case 'keyAccomplishment':
-                section = 'accomplishmentsList';
-                break;
-            case 'strength':
-                section = 'strengthList';
-                break;
-            case 'cert':
-                section = 'certificationsList';
-                break;
-            case 'tech':
-                acc.techExpertiseList[item.subCategory].push(item.value);
-                break;
-        }
+            switch (item.category) {
+                case 'keyAccomplishment':
+                    section = 'accomplishmentsList';
+                    break;
+                case 'strength':
+                    section = 'strengthList';
+                    break;
+                case 'cert':
+                    section = 'certificationsList';
+                    break;
+                case 'tech':
+                    acc.techExpertiseList[item.subCategory].push(item.value);
+                    break;
+            }
 
-        section !== null ? acc[section].push(item.value) : 'section not found';
+            section !== null ? acc[section].push(item.value) : 'section not found';
 
-        return acc;
-    }, resultObj);
+            return acc;
+        }, resultObj);
 
-    return resultObj;
+        return resultObj;
+    }
+    catch (error) {
+        return { error: error }
+    }
 };
 
 
